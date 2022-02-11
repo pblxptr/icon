689480796d0377ba738045a64717cbb47fbd5d00
bielu000 <bielu000@o2.pl>
2022-02-11 20:11:58 +0100

    Fix gitub actions

--- conanfile.py
+++ conanfile.py
@@ -1,84 +1,90 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
-import shutil
-import os
-class IconConan(ConanFile):
-    name = "icon"
-    version = "0.1"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Icon here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {
-      "shared": [True, False],
-      "fPIC": [True, False],
-      "enable_testing": [True, False]
-    }
-    default_options = {
-      "shared": False,
-      "fPIC": True,
-      "enable_testing" : False
-    }
-    generators = [
-      "cmake",
-      "cmake_find_package",
-      "cmake_paths"
-    ]
-    exports_sources = [
-      "conan/CMakeLists.txt",
-      "cmake/*",
-      "icon/*",
-      "CMakeLists.txt",
-      "tests/*"
-    ]
-    requires = [
-      "boost/1.78.0",
-      "cppzmq/4.8.1",
-      "protobuf/3.19.1",
-      "spdlog/1.9.2",
-    ]
+name: CMake
 
-    def requirements(self):
-      if self.options.enable_testing:
-        self.requires("catch2/3.0.0@bhome/testing")
+on:
+  push:
+    branches: [ master, dev ]
+  pull_request:
+    branches: [ master ]
 
-    def source(self):
-      shutil.move("CMakeLists.txt", "CMakeListsOriginal.txt")
-      shutil.move(os.path.join("conan", "CMakeLists.txt"), "CMakeLists.txt")
+env:
+  # Customize the CMake build type here (Release, Debug, RelWithDebInfo, etc.)
+  BUILD_TYPE: Release
 
-    def config_options(self):
-      if self.settings.os == "Windows":
-          del self.options.fPIC
+jobs:
+  build:
+    runs-on: ubuntu-latest
 
-    def validate(self):
-        if self.settings.os == "Windows":
-            raise ConanInvalidConfiguration("Windows not supported")
+    steps:
+    - uses: actions/checkout@v2
 
-        if self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "10":
-            raise ConanInvalidConfiguration("GCC version must be >= 10")
+    - name: Setup GCC-11
+      run: sudo apt install -y --no-install-recommends gcc-11 g++-11
+      
+#     - name: Setup Clang-14
+#       run: sudo apt-get install -y --no-install-recommends \
+#         clang-14 lldb-14 lld-14 clangd-14 \
+#         llvm-14-dev libclang-14-dev clang-tidy-14 \
+#         libc++-dev
 
-        if self.settings.compiler == "clang" and tools.Version(self.settings.compiler.version) <= "13":
-            raise ConanInvalidConfiguration("Clang version must be >= 13")
+    - name: Setup precompiled libraries
+      run: sudo apt install
+        libfmt-dev
+        libspdlog-dev
+        libboost-all-dev
+        protobuf-compiler
+        libprotoc-dev
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.definitions["ENABLE_TESTING"] = self.options.enable_testing
-        cmake.configure()
-        cmake.build()
+    ######### BOOST #########
+    - name: Cache Boost
+      id: cache-boost
+      uses: actions/cache@v2
+      with:
+        path: ${{github.workspace}}/boost_1_78_0
+        key:  boost_1_78_0
 
-        if (self.options.enable_testing):
-          cmake.test();
+    - name: Download and bootstrap Boost
+      if: steps. cache-boost.outputs.cache-hit != 'true'
+      run: wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz
+          && tar -zxf boost_1_78_0.tar.gz
+          && cd boost_1_78_0 && ./bootstrap.sh --prefix=/usr
 
-    def package(self):
-        self.copy("*.h", dst="include/icon", src="icon")
-        self.copy("*.hpp", dst="include/icon", src="icon")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib*", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+    - name: Install Boost
+      run: cd boost_1_78_0 && sudo ./b2 install
+          && cd ${{github.workspace}}
 
-    def package_info(self):
-        self.cpp_info.libs = ["icon"]
+    ######### ZMQ #########
+    - name: Install ZMQ
+      run: git clone https://github.com/zeromq/libzmq.git && cd libzmq
+            && ./autogen.sh
+            && ./configure
+            && sudo make -j4 install
+            && cd ${{github.workspace}}
+
+    ######### CPPZMQ #########
+    - name: Install CPPZMQ
+      run: git clone https://github.com/zeromq/cppzmq.git && cd cppzmq
+            && mkdir build && cd build
+            && cmake ..
+            && sudo make -j4 install
+            && cd ${{github.workspace}}
+
+    - name: Install Catch2
+      run: git clone https://github.com/catchorg/Catch2.git
+          && cd Catch2
+          && cmake -Bbuild -H. -DBUILD_TESTING=OFF
+          && sudo cmake --build build/ --target install
+          && cd ${{github.workspace}}
+
+    - name: Configure CMake
+      run: cmake -B ${{github.workspace}}/build -DCMAKE_BUILD_TYPE=${{env.BUILD_TYPE}} -DENABLE_TESTING=ON
+      env:
+        CC: gcc-11
+        CXX: g++-11
+        BOOST_ROOT: ${{ steps.install-boost.outputs.BOOST_ROOT }}
+
+    - name: Build
+      run: cmake --build ${{github.workspace}}/build --config ${{env.BUILD_TYPE}}
+
+    - name: Test
+      working-directory: ${{github.workspace}}/build
+      run: ctest -C ${{env.BUILD_TYPE}}
